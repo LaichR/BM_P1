1e3b8ac2d076aed8f0d5b73b9abb3f1ddc3a7e45
LaichR <46196385+LaichR@users.noreply.github.com>
2020-09-09 17:24:02 +0200

    Update Histogram.c
    Hi,
    I added some comments to your solution
    best,
    R.Laich

--- Histogram/Histogram/Histogram.c
+++ Histogram/Histogram/Histogram.c
@@ -35,7 +35,8 @@ int main()
 	//fclose(stream);
 
 
-
+	// @RLA: How about the part where you give parameters to your program? it would be nice if
+	// we could give the file to read as an argument!
 
 	// Aufgabe 3
 	FILE* stream = NULL;
@@ -49,14 +50,21 @@ int main()
 
 	assert(stream != NULL);
 	int ch = getc(stream);
-	int histogram[256] = { 0 };
+	// @RLA: such large local variables will be a problem on systems with limited memroy resources since the full variable will be allocated
+	// on the stack; using the keyword static will make this variable a global variable even if you leave the declaration within the funktion
+	// consider using the types from stdint.h instead of int!
+	int histogram[256] = { 0 }; 
+	
 	int maxSizeLetter = 0;
 
 	while (ch > EOF)
 	{
-		if (ch != EOF)
+		if (ch != EOF) // @RLA: this comparison is not needed if you do the histogram computation and the getc in this order
 		{
 			histogram[ch]++;
+			// this will compute the letter that was used most often for this example.
+			// the task was slightly differnt; i asked you to implement a generic funktion that is able 
+			// to find the maximum element in an array. For this example the solution is efficient - but it cannot be reused!   
 			if (histogram[ch] > histogram[maxSizeLetter])
 			{
 				maxSizeLetter = ch;
@@ -71,7 +79,7 @@ int main()
 			printf("Zeichen \'%c\' = %d\n", i, histogram[i]);
 		}
 	}
-	printf("Grösste Anzahl von Buchstabe \'%c\' = %d\n", maxSizeLetter, histogram[maxSizeLetter]);
+	printf("GrÃ¶sste Anzahl von Buchstabe \'%c\' = %d\n", maxSizeLetter, histogram[maxSizeLetter]);
 
 	fclose(stream);
 }
